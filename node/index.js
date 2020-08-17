5f39cbf5b993e02b5633d0d7de13268c012f6331
Raviteja582 <raviteje1028312@gmail.com>
2020-08-17 09:48:29 +0530

    created a simple node file for finding area and perimeter of rectangle.

---
+++ node/index.js
@@ -0,0 +1,18 @@
+var rect={
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y),
+};
+
+function solveRect(l,b) {
+    console.log("Solving for rectangle l= "+l+" breadth b = "+b);
+
+    if(l<=0 || b<=0) console.log("Not possible");
+    else {
+        console.log("Area of rect = "+ rect.area(l,b)); 
+        console.log("Perimeter of rect = "+ rect.perimeter(l,b)); 
+    }
+}
+
+solveRect(0,0);
+solveRect(3,5);
+solveRect(1,10);
