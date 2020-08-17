3841eeb6c5c69a169b21042dff79ad0cf0c13127
Raviteja582 <raviteje1028312@gmail.com>
2020-08-17 10:57:31 +0530

    Setup server to serve the static files in public folder using Express.
    1.We used morgan to print the log files.
    2.We use express.static to serve the statics in the public directory.

--- node-express/index.js
+++ node-express/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname= 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));  /*  So, this is the development version. So, it will print out additional information to the screen as required. */
+
+app.use(express.static(__dirname+ '/public'))     /* I'm going to declare up use and express static. This tells Express to serve up the static files from double underscore dirname. So, this says the root of this project and they will find those files in double_dirname, plus/ public. */
 
 app.use((req,res,next) =>{
-    console.log(req.headers);
     res.statusCode=200;
     res.setHeader('Content-Type','text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>')
