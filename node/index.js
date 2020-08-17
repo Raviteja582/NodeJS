3e3261b54a16e6d156ffbc0d34a426c1275a2a26
Raviteja582 <raviteje1028312@gmail.com>
2020-08-17 09:59:47 +0530

    Implemented Callback feature for the reactangle.js module to understand the Asynchronous programming in JS.

--- node/index.js
+++ node/index.js
@@ -1,14 +1,26 @@
 var rect=require('./rectangle');
-function solveRect(l,b) {
-    console.log("Solving for rectangle l= "+l+" breadth b = "+b);
 
-    if(l<=0 || b<=0) console.log("Not possible");
-    else {
-        console.log("Area of rect = "+ rect.area(l,b)); 
-        console.log("Perimeter of rect = "+ rect.perimeter(l,b)); 
-    }
-}
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
+
 
 solveRect(0,0);
 solveRect(3,5);
 solveRect(1,10);
+solveRect(-3,10);
+solveRect(2,100);
+solveRect(-1,-1);
--- node/rectangle.js
+++ node/rectangle.js
@@ -1,4 +1,23 @@
 /* A simple node module is created */
 
-exports.perimeter = (x,y) => (2*(x+y))
-exports.area= (x,y) => (x*y)
+
+/* Callbacks takes two paramater 1st is the error or success and the value should be return. */
+/* In secound Callback we send value as object contain two functions. */
+/* In set timout funtion we use to call the callback and time to delay the callback. */
+
+
+module.exports = (x,y,callback) => {
+    if (x <= 0 || y <= 0)
+        setTimeout(() => 
+            callback(new Error("Rectangle dimensions should be greater than zero: l = "
+                + x + ", and b = " + y), 
+            null),
+            2000);
+    else
+        setTimeout(() => 
+            callback(null, {
+                perimeter: () => (2*(x+y)),
+                area:() => (x*y)
+            }), 
+            2000);
+}
