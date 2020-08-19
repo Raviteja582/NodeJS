dcc648faa3f4371d4edfc3f02605262d084dd5f2
Raviteja582 <raviteje1028312@gmail.com>
2020-08-20 00:58:32 +0530

    Added comments schema in dishes.js and modify index.js to update and insert comments in the mongodb.

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -9,21 +9,35 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected Corretly to server');
 
-    
-    var newDish =Dishes({
+
+    Dishes.create({
         name: 'Uthapizza',
         description: 'test'
-    });
-
-    newDish.save()
-    .then((dish) =>{
-        console.log(dish+ "Is successfuly inserted");
-
-        return Dishes.find({}).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) =>{
+        console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set: {description: 'Updated Test'}
+        },{
+            new : true 
+        }).exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating : 5,
+            comment: 'Its nice to learning MongoDB',
+            author: 'Ravi teja',
+        });
+
+        return dish.save();
+    })
+    .then((dish) =>{
+
+        console.log(dish);
         return Dishes.remove({});
+
     })
     .then(() => {
         return mongoose.connection.close();
