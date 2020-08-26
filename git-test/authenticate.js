0c2d7e3de4b41c07b7a2fe1e786043c72b39e9d6
Raviteja582 <raviteje1028312@gmail.com>
2020-08-26 20:59:06 +0530

    Added oauth2 for facebook login and index.html is modified for button.

--- git-test/authenticate.js
+++ git-test/authenticate.js
@@ -7,6 +7,11 @@ var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
+
+/* facebook AUth */
+var FacebookTokenStrategy = require('passport-facebook-token');
+
+
 var config = require('./config.js');
 const { NotExtended } = require('http-errors');
 
@@ -42,13 +47,43 @@ exports.verifyAdmin= (req,res,next) => {
         next();
     }
     else{
-        err = new Error(' You are not Authorized to perform this operation! ');
+        var err = new Error(' You are not Authorized to perform this operation! ');
         err.status = 403;
         return next(err);
     }
 }
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+
+/* facebook Auth */
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
+}
+));
+
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
--- git-test/config.js
+++ git-test/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/git-test'
+    'mongoUrl' : 'mongodb://localhost:27017/git-test',
+    'facebook': {
+        clientId: '612448086137545',
+        clientSecret: 'e5b9783febc6a4dcbb61c8da92755e5c',
+    }
 }
